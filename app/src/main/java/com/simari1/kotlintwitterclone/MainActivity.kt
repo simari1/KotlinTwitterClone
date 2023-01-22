984f5b938861510862b5271361450725db13324d
arisima <arito.shimazaki@avanade.com>
2023-01-22 12:52:36 +0900

    Initial commit

---
+++ app/src/main/java/com/simari1/kotlintwitterclone/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.simari1.kotlintwitterclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
